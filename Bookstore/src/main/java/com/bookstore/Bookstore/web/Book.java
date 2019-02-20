94ca4dbafef186f6c9d8b3e004c174ad2d6d5742
tonyastro77 <tonyastro77@users.noreply.github.com>
2019-02-20 15:49:51 +0200

    First commit

---
+++ Bookstore/src/main/java/com/bookstore/Bookstore/web/Book.java
@@ -0,0 +1,43 @@
+package com.bookstore.Bookstore.web;
+
+public class Book {
+	private String title;
+	private String author;
+	private int year;
+	private long isbn;
+	private int price;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public long getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(long isbn) {
+		this.isbn = isbn;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+
+}
